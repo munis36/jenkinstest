46718472ac0bb9082498e2eec305bfdf309596ee
mnagarat <munis36@hotmail.com>
2018-01-06 21:09:39 -0500

    gitignore

--- src/main/java/com/monotonic/testing/m2/Coffee.java
+++ src/main/java/com/monotonic/testing/m2/Coffee.java
@@ -28,6 +28,7 @@ public final class Coffee {
 
     @Override
     public String toString() {
+        System.out.println("Test");
         return "Coffee{" +
                 "type=" + type +
                 "milk=" + milk +
